f6eba9e6f846ec5219f4de3a74279ecb9b3c5bcf
Jake Murphy <murph9y@gmail.com>
2024-01-04 17:58:28 +1100

    improve Graph.cs, manage datasets + autoscale + colours

--- godot/Component/Graph.cs
+++ godot/Component/Graph.cs
@@ -1,4 +1,5 @@
 using Godot;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,24 +9,34 @@ public partial class Graph : HBoxContainer {
 
 	public class Dataset {
         public string GraphName { get; }
-        public float Min { get; }
-        public float Max { get; }
-        internal int CurrentIndex  { get; private set; }
-
-        public Color Color { get; set; } = Colors.Blue;
-
         public readonly float[] Values;
         public readonly Vector2[] GraphPoints;
 
-        public Dataset(string graphName, float min, float max) {
+        public float Min { get; private set; }
+        public float Max { get; private set; }
+        internal int CurrentIndex  { get; private set; }
+
+        public Color Color { get; set; } = Colors.Blue;
+        public bool AutoScale { get; set; } = true;
+
+
+        public Dataset(string graphName, float min = 0, float max = 100, bool autoScale = false) {
             GraphName = graphName;
             Min = min;
             Max = max;
             Values = new float[1090];
+            AutoScale = autoScale;
             GraphPoints = new Vector2[1090]; // enough to fill the screen
         }
 
         public void Push(float value) {
+            // perf, but autoscale is helpful
+            if (AutoScale && (value > Max || value < Min))
+            {
+                Min = Mathf.Min(value, Min);
+                Max = Mathf.Max(value, Max);
+            }
+
             // don't print outside
             var val = Mathf.Clamp(value, Min, Max);
 
@@ -34,33 +45,54 @@ public partial class Graph : HBoxContainer {
         }
     }
 
-    private readonly Vector2 _graphSize;
+    private Vector2 GraphSize { get; }
+    public int Group { get; }
+    private readonly List<Dataset> _datasets;
 
-    public List<Dataset> Datasets { get; set; }
+    public Graph(Vector2 size, int group = 0, IEnumerable<Dataset> datasets = null) {
+        GraphSize = size;
+        Group = group;
+        _datasets = (datasets ?? Array.Empty<Dataset>()).ToList();
+    }
 
-    public Graph(Vector2 size, IEnumerable<Dataset> datasets) {
-        _graphSize = size;
-        Datasets = datasets.ToList();
+    public int DatasetCount() => _datasets.Count;
+
+    public void AddDataset(Dataset dataset) {
+        var box = GetNode<VBoxContainer>("LabelContainer");
+        var l = new Label() { Text = dataset.GraphName };
+        l.AddThemeColorOverride("font_color", dataset.Color);
+        box.AddChild(l);
+        _datasets.Add(dataset);
+    }
+
+    public void RemoveDataset(Dataset dataset) {
+        var box = GetNode<VBoxContainer>("LabelContainer");
+        var data = box.GetChildren().Where(x => x is Label).First(x => ((Label)x).Text == dataset.GraphName);
+        box.RemoveChild(data);
+        _datasets.Remove(dataset);
     }
 
     // https://github.com/WeaverDev/DebugGUIGraph/blob/master/addons/DebugGUI/Windows/GraphWindow.cs#L554
 
     public override void _Ready() {
-        var box = new VBoxContainer();
+        var box = new VBoxContainer() {
+            Name = "LabelContainer"
+        };
         AddChild(box);
-        foreach (var dataset in Datasets) {
-            box.AddChild(new Label() {
+        foreach (var dataset in _datasets) {
+            var l = new Label() {
                 Text = dataset.GraphName,
                 Name = "Label",
                 LabelSettings = new LabelSettings() {
                     FontColor = dataset.Color
                 }
-            });
+            };
+            l.AddThemeColorOverride("font_color", dataset.Color);
+            box.AddChild(l);
         }
         AddChild(new ColorRect() {
-            Color = Colors.Transparent, // not used for display, used for sizing the graph
-            CustomMinimumSize = _graphSize,
-            Size = _graphSize,
+            Color = Colors.Transparent, // not used for display, used for sizing the graph part
+            CustomMinimumSize = GraphSize,
             Name = "GraphRect"
         });
     }
@@ -68,20 +100,30 @@ public partial class Graph : HBoxContainer {
     public override void _Draw()
     {
         var colorRect = GetNode<ColorRect>("GraphRect");
-        DrawRect(GetRect(), new Color(1,1,1,0.2f));
+        var pos = GetRect().End - GetRect().Position;
+        DrawRect(new Rect2(0, 0, pos.X, pos.Y), new Color(0, 0, 0, 0.2f));
 
         var defaultFont = ThemeDB.FallbackFont;
         int defaultFontSize = ThemeDB.FallbackFontSize;
         var offset = new Vector2();
-        foreach (var dataset in Datasets) {
-            DrawString(defaultFont, colorRect.Position + new Vector2(3, defaultFontSize) + offset,
+        foreach (var dataset in _datasets) {
+            DrawString(defaultFont, colorRect.Position + new Vector2(0, defaultFontSize) + offset,
                 float.Round(dataset.Max, 2).ToString(), HorizontalAlignment.Left, -1, defaultFontSize,  dataset.Color);
-            DrawString(defaultFont, colorRect.Position + new Vector2(3, colorRect.Size.Y) + offset,
+            DrawString(defaultFont, colorRect.Position + new Vector2(0, colorRect.Size.Y) + offset,
                 float.Round(dataset.Min, 2).ToString(), HorizontalAlignment.Left, -1, defaultFontSize,  dataset.Color);
+
+            var fraction = Mathf.InverseLerp(dataset.Min, dataset.Max, 0);
+            if (fraction > 0 && fraction < 1 && Math.Abs(fraction - 0.5f) < 0.35f) {
+                // add a 0 label when its inbetween min and max
+                // and prevent overlapping the min/max values a little
+                DrawString(defaultFont, colorRect.Position + new Vector2(3, colorRect.Size.Y * (1-fraction) + defaultFontSize/2) + offset,
+                    "0", HorizontalAlignment.Left, -1, defaultFontSize,  dataset.Color);
+            }
+
             offset += new Vector2(50, 0);
         }
 
-        foreach (var dataset in Datasets) {
+        foreach (var dataset in _datasets) {
             int num = dataset.Values.Length;
             for (int i = 0; i < num; i++)
             {
