7443177352f08eb096734330db9043e0ad02b9fc
Jake Murphy <murph9y@gmail.com>
2024-01-05 21:06:36 +1100

    move world text correctly

--- godot/Utilities/DebugHelper.cs
+++ godot/Utilities/DebugHelper.cs
@@ -6,7 +6,7 @@ namespace murph9.RallyGame2.godot.Utilities;
 public class DebugHelper {
 
     public static WorldText GenerateWorldText(string text, Vector3 position) {
-        var uiScene = GD.Load<PackedScene>("res://Debug/WorldText.tscn");
+        var uiScene = GD.Load<PackedScene>("res://Utilities/WorldText.tscn");
         var instance = uiScene.Instantiate<WorldText>();
         instance.Position = position;
         instance.SetText(text);
