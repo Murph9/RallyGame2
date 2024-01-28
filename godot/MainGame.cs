6cbf5ad23913e143c886bc64feb235f49fcf7eb1
Jake Murphy <murph9y@gmail.com>
2024-01-28 14:49:54 +1100

    cleanup free'y logic of maingame's classes

--- godot/MainGame.cs
+++ godot/MainGame.cs
@@ -1,51 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
 using murph9.RallyGame2.godot.scenes;
+using murph9.RallyGame2.godot.Utilities;
 
 namespace murph9.RallyGame2.godot;
 
 public partial class MainGame : Node {
+	public record SceneDetail(Type Type, PackedScene Scene);
 
-	private IntroScreen _introScreen;
-	private ReadyScreen _readyScreen;
-	private RacingScreen _racingScreen;
-	private ResultsScreen _resultsScreen;
-	private UpgradeScreen _upgradeScreen;
+	private readonly List<SceneDetail> _sceneCache = [
+		new SceneDetail(typeof(IntroScreen), GD.Load<PackedScene>(GodotClassHelper.GetScenePath(typeof(IntroScreen)))),
+		new SceneDetail(typeof(ReadyScreen), GD.Load<PackedScene>(GodotClassHelper.GetScenePath(typeof(ReadyScreen)))),
+		new SceneDetail(typeof(RacingScreen), GD.Load<PackedScene>(GodotClassHelper.GetScenePath(typeof(RacingScreen)))),
+		new SceneDetail(typeof(ResultsScreen), GD.Load<PackedScene>(GodotClassHelper.GetScenePath(typeof(ResultsScreen)))),
+		new SceneDetail(typeof(UpgradeScreen), GD.Load<PackedScene>(GodotClassHelper.GetScenePath(typeof(UpgradeScreen))))
+	];
+
+	private T GetFromCache<T>() where T : Node => _sceneCache.Single(x => x.Type == typeof(T)).Scene.Instantiate<T>();
 
 	public override void _Ready() {
-		_introScreen = GD.Load<PackedScene>("res://scenes/IntroScreen.tscn").Instantiate<IntroScreen>();
-		_readyScreen = GD.Load<PackedScene>("res://scenes/ReadyScreen.tscn").Instantiate<ReadyScreen>();
-		_racingScreen = GD.Load<PackedScene>("res://scenes/RacingScreen.tscn").Instantiate<RacingScreen>();
-		_resultsScreen = GD.Load<PackedScene>("res://scenes/ResultsScreen.tscn").Instantiate<ResultsScreen>();
-		_upgradeScreen = GD.Load<PackedScene>("res://scenes/UpgradeScreen.tscn").Instantiate<UpgradeScreen>();
-
-		_introScreen.Closed += () => {
-			RemoveChild(_introScreen);
-			AddChild(_readyScreen);
-		};
-
-		_readyScreen.Closed += () => {
-			RemoveChild(_readyScreen);
-			AddChild(_racingScreen);
-		};
-
-		_racingScreen.Closed += () => {
-			RemoveChild(_racingScreen);
-			AddChild(_resultsScreen);
-		};
-
-		_resultsScreen.Closed += () => {
-			RemoveChild(_resultsScreen);
-			AddChild(_upgradeScreen);
-		};
-
-		_upgradeScreen.Closed += () => {
-			RemoveChild(_upgradeScreen);
-			AddChild(_readyScreen);
-		};
-
-		AddChild(_introScreen);
+		var intro = GetFromCache<IntroScreen>();
+		intro.Closed += () => IntroClosed(intro);
+		AddChild(intro);
 	}
 
 	public override void _Process(double delta) {
+	}
+
+	private void IntroClosed(IntroScreen intro) {
+		RemoveChild(intro);
+		intro.QueueFree();
+
+		var ready = GetFromCache<ReadyScreen>();
+		ready.Closed += () => ReadyClosed(ready);
+		AddChild(ready);
+	}
+
+	private void ReadyClosed(ReadyScreen ready) {
+		RemoveChild(ready);
+		ready.QueueFree();
+
+		var racing = GetFromCache<RacingScreen>();
+		racing.Closed += () => RacingClosed(racing);
+		AddChild(racing);
+	}
+
+	private void RacingClosed(RacingScreen racing) {
+		RemoveChild(racing);
+		racing.QueueFree();
+
+		var results = GetFromCache<ResultsScreen>();
+		results.Closed += () => ResultsClosed(results);
+		AddChild(results);
+	}
+
+	private void ResultsClosed(ResultsScreen results) {
+		RemoveChild(results);
+		results.QueueFree();
+
+		var upgrade = GetFromCache<UpgradeScreen>();
+		upgrade.Closed += () => UpgradeClosed(upgrade);
+		AddChild(upgrade);
+	}
+
+	private void UpgradeClosed(UpgradeScreen upgrade) {
+		RemoveChild(upgrade);
+		upgrade.QueueFree();
+
+		var ready = GetFromCache<ReadyScreen>();
+		ready.Closed += () => ReadyClosed(ready);
+		AddChild(ready);
 	}
 }
