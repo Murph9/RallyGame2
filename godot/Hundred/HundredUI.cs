fa90d05d37a13bf6fec811b4eb0933ea9a4e9f9c
murph9 <murph9y@gmail.com>
2025-01-19 15:58:11 +1100

    show the minimum speed graph at the top

--- godot/Hundred/HundredUI.cs
+++ godot/Hundred/HundredUI.cs
@@ -6,11 +6,13 @@ namespace murph9.RallyGame2.godot.Hundred;
 public partial class HundredUI : VBoxContainer {
 
     public float TargetDistance { get; set; } = 100 * 1000;
+    public float MinimumSpeed { get; set; } = 50;
     public float DistanceTravelled { get; set; }
     public float SpeedKMH { get; set; }
 
-    public override void _Ready() {
-    }
+    public double MinimumSpeedProgress { get; private set; }
+
+    public override void _Ready() {}
 
     public override void _Process(double delta) {
         var progressBar = GetNode<ProgressBar>("CenterContainer/VBoxContainer/HBoxContainerDistance/ProgressBar");
@@ -20,11 +22,17 @@ public partial class HundredUI : VBoxContainer {
         var progressLabel = GetNode<Label>("CenterContainer/VBoxContainer/HBoxContainerDistance/Label");
         progressLabel.Text = Math.Round(value, 2) + " km";
 
-        var value2 = Math.Round(SpeedKMH, 2);
+        if (SpeedKMH < MinimumSpeed) {
+            MinimumSpeedProgress += delta;
+        } else {
+            MinimumSpeedProgress -= delta;
+        }
+        if (MinimumSpeedProgress < 0) MinimumSpeedProgress = 0;
+
         var progressLabel2 = GetNode<Label>("CenterContainer/VBoxContainer/HBoxContainerSpeed/Label");
-        progressLabel2.Text = value2 + " km/h";
+        progressLabel2.Text = "Target: " + MinimumSpeed + " km/h";
 
         var progressBar2 = GetNode<ProgressBar>("CenterContainer/VBoxContainer/HBoxContainerSpeed/ProgressBar");
-        progressBar2.Value = value2;
+        progressBar2.Value = MinimumSpeedProgress;
     }
 }
