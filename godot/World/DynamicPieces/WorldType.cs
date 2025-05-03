5d70cfdb554ebe519f9e1c478be3292face1cac5
murph9 <murph9y@gmail.com>
2025-05-03 17:20:22 +1000

    update ditch world type to be playable for now

--- godot/World/DynamicPieces/WorldType.cs
+++ godot/World/DynamicPieces/WorldType.cs
@@ -9,13 +9,12 @@ public enum WorldType {
 }
 
 public static class WorldTypeExtensions {
-    record struct WorldTypeDetails(string Description, float RoadWidth);
+    record struct WorldTypeDetails(string Description);
     private readonly static Dictionary<WorldType, WorldTypeDetails> DETAILS = new() {
-        {WorldType.Simple, new("Basic Road", 6)},
-        {WorldType.Simple2, new("Basic road with middle", 10)},
-        {WorldType.Ditch, new("Road with ditches on either side", 4)},
+        {WorldType.Simple, new("Basic Road")},
+        {WorldType.Simple2, new("Basic road with middle")},
+        {WorldType.Ditch, new("Road with ditches on either side")},
     };
 
     public static string Description(this WorldType type) => DETAILS[type].Description;
-    public static float RoadWidth(this WorldType type) => DETAILS[type].RoadWidth;
 }
